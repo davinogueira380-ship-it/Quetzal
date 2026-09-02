76a09c5de29fa03cc454a405d2de7881a010505b
Davi <davinogueira380@gmail.com>
2026-09-02 20:07:07 -0300

    Correção de Bug

--- Quetzal.Application/Servicos/Implementacoes/AmbienteServico.cs
+++ Quetzal.Application/Servicos/Implementacoes/AmbienteServico.cs
@@ -159,7 +159,7 @@ namespace Quetzal.Application.Servicos.Implementacoes
                 if (ambiente == null)
                     return ApiResposta<bool>.Falha("Ambiente não encontrada.");
 
-                if (ambiente.Projeto != null && ambiente.Projeto.Any(p => !p.Ativo || p.Ativo))
+                 if (ambiente.Projeto != null && ambiente.Projeto.Any(p => !p.Ativo || p.Ativo))
                 {
                     return ApiResposta<bool>.Falha("Não é possível excluir um ambiente que esteja sendo utilizado por algum projeto.");
                 }
