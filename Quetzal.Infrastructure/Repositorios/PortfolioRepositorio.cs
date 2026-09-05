d29bd825ad9e57ff219e1c3827dbc3600e5a4882
sthefanny carvalho <sthefannycarvalho01@gmail.com>
2026-09-04 21:02:38 -0300

    Refatora nomes e remove métodos obsoletos de projeto
    Renomeia variáveis e métodos de "projeto" para "portfolio" para padronização. Remove métodos relacionados a "projeto", incluindo ObterProjetoDestaqueAsync. Atualiza filtro para FiltrarPorAmbienteAsync. Ajusta mensagens de retorno para português correto. Atualiza IPortfolioServico removendo métodos obsoletos e adicionando comentários para futuras implementações. Adiciona comentários em PortfolioRepositorio e corrige pequenos erros de digitação e comentários.

--- Quetzal.Infrastructure/Repositorios/PortfolioRepositorio.cs
+++ Quetzal.Infrastructure/Repositorios/PortfolioRepositorio.cs
@@ -1,10 +1,50 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Quetzal.Domain.Entidades;
+using Quetzal.Domain.Interfaces;
+using Quetzal.Infrastructure.Dados;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Quetzal.Infrastructure.Repositorios
 {
-    internal class PortfolioRepositorio
-    {
-    }
+    //public class PortfolioRepositorio : IPortfolioRepositorio
+    //{
+    //    private readonly QuetzalContexto _context;
+
+    //    public PortfolioRepositorio(QuetzalContexto context)
+    //    {
+    //        _context = context;
+    //    }
+
+    //    public async Task<IEnumerable<Portfolio>> ObterTodosAsync(bool incluirInativos = false)
+    //    {
+    //        IQueryable<Portfolio> query = _context.Portfolios.Include(Portfolio => Portfolio.Ambiente);
+
+    //        // Se incluirInativos for false, filtra apenas os portfolios ativos
+    //        if (!incluirInativos)
+    //        {
+    //            query = query.Where(Portfolio => Portfolio.Ativo);
+    //        }
+    //        return await query.ToListAsync();
+    //    }
+
+    //    public async Task<Portfolio?> ObterPorIdAsync(int id)
+    //    {
+    //        return await _context.Portfolios.Include(Portfolio => Portfolio.Ambiente).FirstOrDefaultAsync(Portfolio => Portfolio.Id == id);
+    //    }
+
+    //    public async Task<IEnumerable<Portfolio>> FiltrarPorAmbienteAsync(bool incluirInativos, int? ambienteId = null)
+    //    {
+    //        var query = _context.Portfolios.Include(Portfolio => Portfolio.Ambiente).AsQueryable();
+    //        if (ambienteId.)
+    //        {
+                
+    //        }
+    //    }
+
+
+
+
+    //}
 }
